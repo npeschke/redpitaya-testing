744f105c7eb2eb89b914830cd98888a1a4aa5e69
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-07-30 15:01:50 +0200

    added set_channel_gain function

--- api/acq.py
+++ api/acq.py
@@ -6,6 +6,8 @@ Date        30.07.2019
 import redpitaya_scpi as scpi
 
 DECIMATIONS = [1, 8, 64, 1024, 8192, 65535]
+CHANNELS = [1, 2]
+GAINS = ["HV", "LV"]
 
 
 def start_acquisition(rp_s: scpi.scpi):
@@ -30,7 +32,25 @@ def set_decimation(rp_s: scpi.scpi, dec: int):
     :param rp_s: scpi connection object to RedPitaya
     :param dec: Decimation, must be in [1, 8, 64, 1024, 8192, 65535]
     """
+    # Check input
     if dec not in DECIMATIONS:
-        raise AttributeError(f"Your specified decimation ({dec}) is not in the list of poosible decimations ({DECIMATIONS})")
+        raise AttributeError(f"Your specified decimation ({dec}) is not in the list of possible decimations ({DECIMATIONS})!")
 
     rp_s.tx_txt(f"ACQ:DEC {dec}")
+
+
+def set_channel_gain(rp_s: scpi.scpi, channel: int, gain: str):
+    """
+    Sets gain (HV/LV) for a channel. To be used together with the jumper settings of the channels.
+    :param rp_s: scpi connection object to RedPitaya
+    :param channel: Channel, either 1 or 2
+    :param gain: Gain, either HV for full range +-20V or LV for +-!V
+    """
+    # Check input
+    if gain not in GAINS:
+        raise AttributeError(f"Your specified gain ({gain}) is not in the list of possible gains ({GAINS})!")
+
+    if channel not in CHANNELS:
+        raise AttributeError(f"Your specified channel ({channel}) is not in the list of possible channels ({CHANNELS})!")
+
+    rp_s.tx_txt(f"ACQ:SOUR{channel}:GAIN {gain}")
