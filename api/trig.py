cea30a41837ac334bbade10524cc8aef7f81fd08
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-07-30 11:37:58 +0200

    added wait_for_trigd

--- api/trig.py
+++ api/trig.py
@@ -2,3 +2,16 @@
 Author      Nicolas Peschke
 Date        30.07.2019
 """
+
+import redpitaya_scpi as scpi
+
+
+def wait_for_trigd(rp_s: scpi.scpi):
+    """
+    Waits until RedPitaya reports trig'd
+    :param rp_s: scpi connection to RedPitaya
+    """
+    while True:
+        rp_s.tx_txt(f"ACQ:TRIG:STAT?")
+        if rp_s.rx_txt() == "TD":
+            break
