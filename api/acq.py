ce3820d17748a9b8a4d4da81e4919d00bde3824d
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-07-30 14:40:52 +0200

    added docstring to set_decimation

--- api/acq.py
+++ api/acq.py
@@ -7,6 +7,7 @@ import redpitaya_scpi as scpi
 
 DECIMATIONS = [1, 8, 64, 1024, 8192, 65535]
 
+
 def reset_acquisition(rp_s: scpi.scpi):
     """
     Resets oscilloscope
@@ -16,6 +17,11 @@ def reset_acquisition(rp_s: scpi.scpi):
 
 
 def set_decimation(rp_s: scpi.scpi, dec: int):
+    """
+    Sets the decimation for the acquisition and thus the sampling rate and buffer length.
+    :param rp_s: scpi connection object to RedPitaya
+    :param dec: Decimation, must be in [1, 8, 64, 1024, 8192, 65535]
+    """
     if dec not in DECIMATIONS:
         raise AttributeError(f"Your specified decimation ({dec}) is not in the list of poosible decimations ({DECIMATIONS})")
 
