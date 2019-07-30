f457ead17651103487a3e7f8be22dcdb8528da27
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-07-30 14:21:41 +0200

    updated docstring

--- api/trig.py
+++ api/trig.py
@@ -9,7 +9,7 @@ import redpitaya_scpi as scpi
 def wait_for_trigd(rp_s: scpi.scpi):
     """
     Waits until RedPitaya reports trig'd
-    :param rp_s: scpi connection to RedPitaya
+    :param rp_s: scpi connection object to RedPitaya
     """
     while True:
         rp_s.tx_txt(f"ACQ:TRIG:STAT?")
