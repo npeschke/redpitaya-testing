0f068297f267e27495178d9c612a5976176ba7e0
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-07-30 16:37:28 +0200

    initial working version

--- eval_sipm.py
+++ eval_sipm.py
@@ -4,8 +4,8 @@ Date        30.07.2019
 """
 
 import argparse
+
 import matplotlib.pyplot as plt
-import numpy as np
 
 import redpitaya_scpi as scpi
 
@@ -15,11 +15,29 @@ import api.acq as acq
 
 
 def eval_sipm(rp_s: scpi.scpi):
-    # Setup acquisiton
+    # Setup acquisition
     acq.reset_acquisition(rp_s)
     acq.set_decimation(rp_s, 64)
-    trig.
+    trig.set_trigger_level(rp_s, 0)
+    trig.set_trigger_delay(rp_s, 0)
+    acq.set_channel_gain(rp_s, 1, "HV")
+    acq.set_channel_gain(rp_s, 2, "HV")
 
+    acq.start_acquisition(rp_s)
+
+    trig.set_trigger_mode(rp_s, "CH2_PE")
+
+    gen.pulse_slow_analog_out(rp_s, aout=0, voltage=1.8, duration=1)
+    trig.wait_for_trigd(rp_s)
+
+    data_ch1 = acq.get_channel_data(rp_s, 1)
+    data_ch2 = acq.get_channel_data(rp_s, 2)
+
+    # data = pd.DataFrame({"CH1": data_ch1, "CH2": data_ch2})
+    plt.plot(data_ch1)
+    plt.plot(data_ch2, "--")
+
+    plt.show()
 
 
 if __name__ == '__main__':
