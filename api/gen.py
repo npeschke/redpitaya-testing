47d9c53b7761cbb4102f49f1630a38c61fdb0ca0
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-07-30 14:23:14 +0200

    added reset_gen function

--- api/gen.py
+++ api/gen.py
@@ -2,3 +2,13 @@
 Author      Nicolas Peschke
 Date        30.07.2019
 """
+
+import redpitaya_scpi as scpi
+
+
+def reset_gen(rp_s: scpi.scpi):
+    """
+    Resets the signal generator
+    :param rp_s: scpi connection object to RedPitaya
+    """
+    rp_s.tx_txt("GEN:RST")
